7cd927fa0b350bf4dfe99975866d97844d474fbe
MyUsername <romamargine@gmail.com>
2022-04-12 12:00:12 +0200

    Add snack - 3

---
+++ snack-3/main.js
@@ -0,0 +1,15 @@
+/* 
+Scrivi una funzione che accetti una stringa come argomento 
+e la ritorni girata (es. Ciao -> oaiC)
+
+*/
+
+function reverseString(string) {
+    return string.split('').reverse().join('')
+}
+
+console.log(reverseString('hello'));
+
+console.log(reverseString('ciliegia'));
+console.log(reverseString('roma'));
+
